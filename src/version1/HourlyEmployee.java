d77b209fd139b11740d8f7ab281b0c39d4d591cc
mileszc <ramneemiles06@gmail.com>
2026-09-08 11:51:32 +0800

    not finished

---
+++ src/version1/HourlyEmployee.java
@@ -0,0 +1,75 @@
+package version1;
+
+public class HourlyEmployee {
+
+    private int empID;
+    private String empName;
+    private float totalHoursWorked;
+    double ratePerHour;
+
+    HourlyEmployee (){
+        this.empID = 0;
+        this.empName = "N/A";
+        this.totalHoursWorked = 0;
+        this.ratePerHour = 0;
+
+    }
+
+    public HourlyEmployee(int empID, String empName, float totalHoursWorked, double ratePerHour) {
+        this.empID = empID;
+        this.empName = empName;
+        this.totalHoursWorked = totalHoursWorked;
+        this.ratePerHour = ratePerHour;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public float getTotalHoursWorked() {
+        return totalHoursWorked;
+    }
+
+    public void setTotalHoursWorked(float totalHoursWorked) {
+        this.totalHoursWorked = totalHoursWorked;
+    }
+
+    public double getRatePerHour() {
+        return ratePerHour;
+    }
+
+    public void setRatePerHour(double ratePerHour) {
+        this.ratePerHour = ratePerHour;
+    }
+
+    public double computeSalary () {
+        //5 days 8 hours = 40 hours
+
+        double salary = totalHoursWorked * ratePerHour;
+        double otSalary = (totalHoursWorked - 40 * ratePerHour * 1.5);
+        if(totalHoursWorked <= 40) {
+            return salary;
+        } else {
+            return (otSalary + salary);
+        }
+
+
+
+
+    }
+
+
+}
+
